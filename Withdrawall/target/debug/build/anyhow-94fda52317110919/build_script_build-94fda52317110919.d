3831ad7a7e094a18b1b3a72820f2c2e58b0c4d3a
PANDA <148841983+Panda404NotFound@users.noreply.github.com>
2024-05-10 08:21:37 -0700

    Delete Withdrawall/target directory

--- Withdrawall/target/debug/build/anyhow-94fda52317110919/build_script_build-94fda52317110919.d
+++
@@ -1,5 +0,0 @@
-/Users/a1111/Desktop/GiftForDragon/GiftForDragon/Withdrawall/target/debug/build/anyhow-94fda52317110919/build_script_build-94fda52317110919: /Users/a1111/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anyhow-1.0.82/build.rs
-
-/Users/a1111/Desktop/GiftForDragon/GiftForDragon/Withdrawall/target/debug/build/anyhow-94fda52317110919/build_script_build-94fda52317110919.d: /Users/a1111/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anyhow-1.0.82/build.rs
-
-/Users/a1111/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anyhow-1.0.82/build.rs:
--- Withdrawall/target/debug/deps/withdrawall-ccff68d8b3f2d070.d
+++
@@ -1,5 +0,0 @@
-/Users/a1111/Desktop/GiftForDragon/GiftForDragon/Withdrawall/target/debug/deps/libwithdrawall-ccff68d8b3f2d070.rmeta: src/main.rs
-
-/Users/a1111/Desktop/GiftForDragon/GiftForDragon/Withdrawall/target/debug/deps/withdrawall-ccff68d8b3f2d070.d: src/main.rs
-
-src/main.rs:
